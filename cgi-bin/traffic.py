486c68dea8991373063fb36bf439de8f58065be4
Thor J. Kooda <792089+tkooda@users.noreply.github.com>
2023-10-24 03:11:06 -0500

    make traffic-stats use default interface

--- cgi-bin/traffic.py
+++ cgi-bin/traffic.py
@@ -9,7 +9,6 @@ import subprocess
 
 DIR_IMG = "/var/www/html/rrd/image/"
 URL_BASE = "/rrd/image"
-IFACE = "eth0"
 
 # period name and seconds to cache old png ..
 PERIODS = [
@@ -22,24 +21,40 @@ PERIODS = [
 	( "year", 86400 ),
 	]
 
+
+def get_default_iface_name_linux():
+	route = "/proc/net/route"
+	with open(route) as f:
+		for line in f.readlines():
+			try:
+				iface, dest, _, flags, _, _, _, _, _, _, _, =  line.strip().split()
+				if dest != '00000000' or not int(flags, 16) & 2:
+					continue
+				return iface
+			except:
+				continue
+
+
+iface = get_default_iface_name_linux()
+
 print( """\
 HTTP/1.0 200 OK
 Content-type: text/html
 
 <html>
 <body>
-The Blue graph shows traffic leaving the interface (from the interface's point of view), the Green graph (grows downward) shows traffic entering the interface.<br/>
+The Blue graph shows traffic leaving the interface (from the interface's point of view), the Green graph (grows downward) shows traffic entering the {} interface.<br/>
 <br/>
-""" )
+""".format( iface )  )
 
 for period_name, period_min in PERIODS:
-    fname = "%s-traffic-%s.png" % ( IFACE, period_name )
+    fname = "%s-traffic-%s.png" % ( iface, period_name )
     fpath = os.path.join( DIR_IMG, fname )
     
     if not os.path.exists( fpath ) or \
             os.stat( fpath )[8] + period_min < time.time():
         subprocess.call( [ "/etc/sv/traffic-stats/bin/make-graphs",
-                           IFACE, period_name ], stdout=open('/dev/null', 'w') )
+                           iface, period_name ], stdout=open('/dev/null', 'w') )
     
     url = os.path.join( URL_BASE, fname )
     print( """<a name="%s">%s</a> :<br/><img src="%s"><br/><br/><br/>""" % ( period_name, period_name, url ) )
